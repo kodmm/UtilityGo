8b464138d920ed1851aab59cb885612d79367ab5
kodmm <53547701+kodmm@users.noreply.github.com>
2022-09-22 04:36:35 +0900

    1.6.1 別名の関数によるオプション引数

---
+++ udon.go
@@ -0,0 +1,49 @@
+package main
+
+type Portion int
+
+const (
+	Regular Portion = iota
+	Small
+	Large
+)
+
+type Udon struct {
+	men      Portion
+	aburaage bool
+	ebiten   uint
+}
+
+func NewUdon(p Portion, aburaage bool, ebiten uint) *Udon {
+	return &Udon{
+		men:      p,
+		aburaage: aburaage,
+		ebiten:   ebiten,
+	}
+}
+
+var tempuraUdon = NewUdon(Large, false, 2)
+
+func NewKakeUdon(p Portion) *Udon {
+	return &Udon{
+		men:      p,
+		aburaage: false,
+		ebiten:   0,
+	}
+}
+
+func NewKitsuneUdon(p Portion) *Udon {
+	return &Udon{
+		men:      p,
+		aburaage: true,
+		ebiten:   0,
+	}
+}
+
+func NewTempuraUdon(p Portion) *Udon {
+	return &Udon{
+		men:      p,
+		aburaage: false,
+		ebiten:   3,
+	}
+}
