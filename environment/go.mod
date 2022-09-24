d1f0d4543cc29cebf4cccc530d926655e1113276
kodmm <53547701+kodmm@users.noreply.github.com>
2022-09-24 16:44:37 +0900

    1.7.2 環境変数

---
+++ environment/go.mod
@@ -0,0 +1,5 @@
+module environment
+
+go 1.19
+
+require github.com/kelseyhightower/envconfig v1.4.0
---
+++ environment/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Port      uint16 `envconfig:"PORT" default:"3000"`
+	Host      string `envconfig:"HOST" required:"true"`
+	AdminPort uint16 `envconfig:"ADMIN_PORT" default:"3001"`
+}
+
+func main() {
+	var c Config
+	err := envconfig.Process("", &c)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	log.Println(c)
+}
