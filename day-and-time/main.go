34ee9f1a8212a29ad31099112962117f11706ef1
kodmm <53547701+kodmm@users.noreply.github.com>
2022-09-25 09:38:45 +0900

    1.10.2 時間をあらわすtime.Duration

--- day-and-time/main.go
+++ day-and-time/main.go
@@ -19,4 +19,31 @@ func main() {
 
 	fmt.Println(now2)
 	fmt.Println(past)
+
+	fiveMinute := 5 * time.Minute
+	fmt.Println(now.Add(fiveMinute))
+	fmt.Println("*************")
+	// intとは型違いで直接演算できないので、即値との計算以外は
+	// time.Durationへの明示的なキャストが必要
+	// キャストがないとエラーが発生する。
+	// invalid operation: seconds * time.Second(mismatched types int and time.Duration)
+	const seconds int = 10
+	timeSeconds := time.Duration(seconds) * time.Second
+
+	// Timeの演算でduration作成
+	dur := now.Sub(past)
+
+	fmt.Println((timeSeconds))
+	fmt.Println(dur)
+
+	// 1時間にまとめてパッチで読み込むファイル名を取得
+	filepath := time.Now().Truncate(time.Minute).Format("20060102150405.json")
+
+	// 5分ごと5分前の時刻
+	fiveMinuteAfter := time.Now().Add(fiveMinute)
+	fiveMinuteBefore := time.Now().Add(-fiveMinute)
+
+	fmt.Println("filepath", filepath)
+	fmt.Println("fiveMinuteBefore", fiveMinuteBefore)
+	fmt.Println("fiveMinuteAfter", fiveMinuteAfter)
 }
