01cf68610d3ab80731ad0b5baa0c9e8aceb900dc
kodmm <53547701+kodmm@users.noreply.github.com>
2022-10-05 05:49:28 +0900

    2.2.1 メソッドを追加して組み込み型を拡張する

--- second-chapter/main.go
+++ second-chapter/main.go
@@ -23,5 +23,41 @@ func main() {
 
 	fileType := reflect.TypeOf((*os.File)(nil))
 	fmt.Println(fileType.Implements(writeType))
+	fmt.Println("status: ", StatusForbidden)
+	fmt.Println("route: ", AizuNumataKaido)
 
+}
+
+//go:generate enumer --type=HTTPStatus -json
+type HTTPStatus int
+
+const (
+	StatusOK              HTTPStatus = 200
+	StatusUnauthorized    HTTPStatus = 401
+	StatusPaymentRequired HTTPStatus = 402
+	StatusForbidden       HTTPStatus = 403
+)
+
+type NationalRoute int
+
+const (
+	NagasakiKaido   NationalRoute = 200
+	AizuNumataKaido NationalRoute = 401
+	HokurikuDo      NationalRoute = 402
+	KurinokiBypass  NationalRoute = 403
+)
+
+func (n NationalRoute) String() string {
+	switch n {
+	case NagasakiKaido:
+		return "長崎街道"
+	case AizuNumataKaido:
+		return "会津沼田街道"
+	case HokurikuDo:
+		return "北陸道"
+	case KurinokiBypass:
+		return "栗の木バイパス"
+	default:
+		return fmt.Sprintf("国道%d号線", n)
+	}
 }
