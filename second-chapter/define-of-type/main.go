6299c8b86bb5eece330e8b6d109b1ff07fde341d
kodmm <53547701+kodmm@users.noreply.github.com>
2022-10-05 23:45:49 +0900

    列挙への型定義

--- second-chapter/define-of-type/main.go
+++ second-chapter/define-of-type/main.go
@@ -37,3 +37,17 @@ func (c SKUCode) ColorCD() string{
 	itemCD, sizeCD, colorCD, := skuCD.ItemCD(), skuCD.SizeCD(), skuCD.ColorCD()
 }
 
+type Season int
+
+const(
+	Peak Season = iota + 1 // 繁忙期
+	Normal // 通常期
+	Off // 閑散期
+)
+
+func (s Season) Price(price float64) bool {
+	if s == Peak {
+		return s + 200
+	}
+	return s
+}
