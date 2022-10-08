e13e5c6bf96f291b5030106c80bac21d9a4e50fc
kodmm <53547701+kodmm@users.noreply.github.com>
2022-10-09 01:56:50 +0900

    クロージャを使って使ってメソッドを実現する

--- third-chapter/web/main.go
+++ third-chapter/web/main.go
@@ -26,6 +26,24 @@ func register(h *Handler) http.Handler {
 	return mux
 }
 
+func register(db *sql.DB) http.Handler {
+	mux := http.NewServeMux()
+
+	// 関数定義は関数ない関数で実装
+	// 以前レシーバ経由で使っていたDB Connectionは
+	// クロージャがキャプチャした関数定義外のregister()の引数を利用
+	mux.HandleFunc("/value", func(w http.ResponseWriter, r *http.Request) {
+		var v Value
+		err := db.Get(&v, "SELECT * FROM person WHERE id=$1", r.URL.Query().Get("id"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		json.NewEncoder(w).Encode(v)
+	})
+	return mux
+}
+
 func main() {
 
 }
