54ece12f92052e5ff57e7d9e66cfc7e3cc413232
kodmm <53547701+kodmm@users.noreply.github.com>
2022-10-10 22:54:44 +0900

    3.6.3 ミュータブルな構造体とイミュータブルな構造体

--- third-chapter/pointer-example/main.go
+++ third-chapter/pointer-example/main.go
@@ -1,21 +1,24 @@
-pacakge main
+package main
+
+import "math/big"
 
 // ポインターとしてのみ利用する構造体
 type NoCopyStruct struct {
-	self *NoCopyStruct
+	self  *NoCopyStruct
 	Value *string
 }
 
 // 初期化時にポインターを保持しておく
 func NewNoCopyStruct(value string) *NoCopyStruct {
 	r := &NoCopyStruct{
-		Value: &value
+		Value: &value,
 	}
 	r.self = r
 	return r
 }
+
 // メソッドの中でチェック
-func (n *NoCopyStruct) String() {
+func (n *NoCopyStruct) String() string {
 	if n != n.self {
 		panic("should not copy NoCopyStruct intstance without Copy() method")
 	}
@@ -26,11 +29,41 @@ func (n *NoCopyStruct) String() {
 func (n *NoCopyStruct) Copy() *NoCopyStruct {
 	str := *n.Value
 	p2 := &NoCopyStruct{
-		Value: &str
+		Value: &str,
 	}
 	p2.self = p2
 	return p2
 }
+
+type MutableMoney struct {
+	currency Currency
+	amount   *big.Int
+}
+
+func (m MutableMoney) Currency() Currency {
+	return m.currency
+}
+
+func (m *MutableMoney) SetCurrency(c Currency) {
+	m.Currency = c
+}
+
+type ImmutableMoney struct {
+	currency Currency
+	amount   *big.Int
+}
+
+func (im ImmutableMoney) Currency() Currency {
+	return im.currency
+}
+
+func (im ImmutableMoney) SetCurrency(c Currency) ImmutableMoney {
+	return ImmutableMoney{
+		currency: c,
+		amount:   im.amount,
+	}
+}
+
 func main() {
 
 }
