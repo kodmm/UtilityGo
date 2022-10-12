187d25f1467c56ee7b32e0b81182d3f659f20840
kodmm <53547701+kodmm@users.noreply.github.com>
2022-10-12 16:49:04 +0900

    3.8 構造体のメモリ割り当てを高速化する

--- third-chapter/channel-example/main.go
+++ third-chapter/channel-example/main.go
@@ -3,7 +3,20 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"sync"
 )
+
+// 巨大な構造体
+type BigStruct struct {
+	Member string
+}
+
+// Poolは初期化関数をNewフィールドに設定して作成する
+var pool = &sync.Pool{
+	New: func() interface{} {
+		return &BigStruct{}
+	},
+}
 
 func main() {
 	emptyStructType := reflect.TypeOf(&struct{}{}).Elem()
@@ -19,4 +32,25 @@ func main() {
 	fmt.Println("受信待ち")
 	<-wait
 	fmt.Println("受信完了")
+
+	// インスタンスはGet()メソッドで取得
+	// 在庫があればそれ、なければNew()を呼び出す
+	b := pool.Get().(*BigStruct)
+
+	// 使い終わったらPut()でPoolに戻して次回に備える
+	pool.Put(b)
+}
+
+// BigStructのインスタンスを作成するファクトリー関数
+// 内部でプールを利用
+func NewBigStruct() *BigStruct {
+	b := pool.Get().(*BigStruct)
+	return b
+}
+
+// 自分自身を返却するメソッド
+func (b *BigStruct) Release() {
+	// 初期化してから格納
+	b.Member = ""
+	pool.Put(b)
 }
