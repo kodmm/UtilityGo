77d55cafffc48107045e9f505a6108a3ec3d646f
kodmm <53547701+kodmm@users.noreply.github.com>
2022-10-18 05:51:44 +0900

    fmt.Errorf

--- five-chapter/main.go
+++ five-chapter/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var ErrNotFound = errors.New("not found")
 
@@ -8,6 +11,14 @@ func findBook(isbn string) (*Book, error) {
 	// ...
 	// 値が取得できなかったため、ErrNotFoundを返す
 	return nil, ErrNotFound
+}
+
+func validate(length int) error {
+	if length <= 0 {
+		return fmt.Errorf("length must be greater than0, length = %d", length)
+	}
+	// ...
+	return nil
 }
 
 func main() {
