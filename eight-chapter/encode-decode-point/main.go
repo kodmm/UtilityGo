abeb0c533156a32981adacae8575db420209f23e
kodmm <53547701+kodmm@users.noreply.github.com>
2022-11-15 08:08:40 +0900

    UnmarshalJSONやMarshalJSONを使ったJSONエンコード/デコード拡張

--- eight-chapter/encode-decode-point/main.go
+++ eight-chapter/encode-decode-point/main.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"strconv"
+	"time"
 )
 
 type user struct {
@@ -28,6 +30,13 @@ type Rectangle struct {
 	Width  int `json:"width"`
 	Height int `json:"height"`
 }
+
+type Record struct {
+	ProcessID string `json:"process_id"`
+	DeletedAt JSTime `json:"deleted_at"`
+}
+
+type JSTime time.Time
 
 func main() {
 	u := user{
@@ -77,11 +86,48 @@ func main() {
 
 	if err := d.Decode(&rect); err != nil {
 		// Error Handling
+	}
+
+	r := &Record{
+		ProcessID: "001",
+		DeletedAt: JSTime(time.Now()),
+	}
+
+	e, _ := json.Marshal(r)
+	fmt.Println(string(e))
+
+	var t *Record
+	if err := json.Unmarshal(e, &t); err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("%+v\n", time.Time(t.DeletedAt).Format(time.RFC3339Nano))
 
 }
 
 func Int(v int) *int {
 	return &v
+}
+
+func (t JSTime) MarshalJSON() ([]byte, error) {
+	tt := time.Time(t)
+	if tt.IsZero() {
+		return []byte("null"), nil
+	}
+	v := strconv.Itoa(int(tt.UnixMilli()))
+	return []byte(v), nil
+}
+
+func (t *JSTime) UnmarshalJSON(data []byte) error {
+	var jsonNumber json.Number
+	err := json.Unmarshal(data, &jsonNumber)
+	if err != nil {
+		return err
+	}
+	unix, err := jsonNumber.Int64()
+	if err != nil {
+		return err
+	}
+
+	*t = JSTime(time.Unix(0, unix))
+	return nil
 }
