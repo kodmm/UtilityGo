be62c8032810d611146d8a728a4b5c1733dfb54e
kodmm <53547701+kodmm@users.noreply.github.com>
2022-11-16 17:13:39 +0900

    8.2.2 CSV形式でファイルに書き込む

--- eight-chapter/csv-project/main.go
+++ eight-chapter/csv-project/main.go
@@ -28,4 +28,33 @@ func main() {
 		}
 		fmt.Println(record)
 	}
+
+	records := [][]string{
+		{"書籍名", "出版年", "ページ数"},
+		{"Golang", "2016", "280"},
+		{"Python", "2018", "256"},
+		{"Node.js", "2018", "316"},
+	}
+
+	file, err := os.OpenFile("oreilly.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	w := csv.NewWriter(file)
+
+	// 区切り文字
+	w.Comma = '\t'
+	defer w.Flush()
+
+	for _, record := range records {
+		if err := w.Write(record); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if err := w.Error(); err != nil {
+		log.Fatal(err)
+	}
 }
