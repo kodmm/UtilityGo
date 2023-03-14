e4e6688d1f8cb994485b7e64f7936f81781dfd1b
kodmm <53547701+kodmm@users.noreply.github.com>
2023-03-15 02:10:52 +0900

    11.1.3 http.Clientを作成してリクエスト

--- chapter-eleven/main.go
+++ chapter-eleven/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 )
 
 type User struct {
@@ -28,4 +30,21 @@ func main() {
 	}
 
 	defer resp.Body.Close()
+
+	client := &http.Client{
+		Timeout:   10 * time.Second,
+		Transport: http.DefaultTransport,
+	}
+
+	// Getリクエストの生成
+	ctx := context.Background()
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://example.com", nil)
+	if err != nil {
+		// ...
+	}
+	// リクエストヘッダーにトークンを付与
+	req.Header.Add("Authorization", "Bearer XXX ... XXX")
+
+	// HTTPリクエストの発行
+	response, err := client.Do(req)
 }
