00d49106c1b8fc68bffad069d962e17d5b5174a1
kodmm <53547701+kodmm@users.noreply.github.com>
2023-03-19 06:35:37 +0900

    13.2.1 Table Driven Testとは

--- chapter-thirteen/simple-test/testsample_test.go
+++ chapter-thirteen/simple-test/testsample_test.go
@@ -1,6 +1,9 @@
 package testsample
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Add(a, b int) int {
 	return a + b
@@ -11,4 +14,21 @@ func TestAdd(t *testing.T) {
 	if got != 3 {
 		t.Errorf("expect 3, but %d", got)
 	}
+}
+
+func Calc(a, b int, operator string) (int, error) {
+	switch operator {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		if b == 0 {
+			return 0, fmt.Errorf("0の除算は未定義です")
+		}
+		return a / b, nil
+	}
+	return 0, fmt.Errorf("予期しない演算子 %v が設定されました。", operator)
 }
