d09e6fc30cbf7914750f2509f12da384fa0aad74
kodmm <53547701+kodmm@users.noreply.github.com>
2023-03-21 11:44:02 +0900

    13.5.2 ハンドラー単体のテスト

--- chapter-thirteen/all-server/main.go
+++ chapter-thirteen/all-server/main.go
@@ -10,14 +10,16 @@ import (
 
 func initServer() http.Handler {
 	r := chi.NewRouter()
-	r.Get("/fortune", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
-		io.WriteString(w, `{"fortune": "大吉"}`)
-	})
+	r.Get("/fortune", fortuneHandler)
 	return r
 }
 
 func main() {
 	log.Println("open at 8080")
 	log.Println(http.ListenAndServe(":8080", initServer()))
+}
+
+func fortuneHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	io.WriteString(w, `{"fortune": 大吉"}`)
 }
--- chapter-thirteen/all-server/main_test.go
+++ chapter-thirteen/all-server/main_test.go
@@ -23,5 +23,20 @@ func TestServerRequest(t *testing.T) {
 	if j["fortune"] != "大吉" {
 		t.Errorf("result should be 大吉, but %s", j["fortune"])
 	}
+}
+
+// ミドルウェアを避けるときに最適
+func TestHandler(t *testing.T) {
+	r := httptest.NewRequest(http.MethodGet, "/fortune", nil)
+	w := httptest.NewRecorder()
+	fortuneHandler(w, r)
+	var j map[string]string
+	if err := json.Unmarshal(w.Body.Bytes(), &j); err != nil {
+		t.Errorf("json unmarshal err should be nil: %v", err)
+		return
+	}
+	if j["fortune"] != "大吉" {
+		t.Errorf("result should be 大吉, but %s", j["fortune"])
+	}
 
 }
