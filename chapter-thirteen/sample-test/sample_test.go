545fa1931ca203ce80bf4a0eab91f73aeb7a182c
kodmm <53547701+kodmm@users.noreply.github.com>
2023-03-22 17:07:51 +0900

    13.9.2 変換が必要な入出力のテスト

--- chapter-thirteen/sample-test/sample_test.go
+++ chapter-thirteen/sample-test/sample_test.go
@@ -1,9 +1,51 @@
 package sample
 
+import (
+	"net/http"
+	"testing"
+)
+
 func TestConsoleOut(t *testing.T) {
 	var b bytes.Buffer
 	DumpUserTo(&b, &User{Name: "クライド・バロウ"})
 	if b.String() != "クライド・バロウ(住所不定)" {
 		t.Errorf("error (expected: 'クライド・バロウ(住所不定)', actual='%s')", b.String())
 	}
+}
+
+type testTransport struct {
+	req **http.Request
+	res *http.Response
+	err error
+}
+
+func (t *testTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	*(t.req) = req
+	return t.res, t.err
+}
+
+var _ http.RoundTripper = &testTransport{}
+
+func newTransport(req **http.Request, res *http.Response, err error) http.RoundTripper {
+	return &testTransport{
+		req: req,
+		res: res,
+		err: err,
+	}
+}
+
+// テストコード
+func TestHTTPRequest(t *testing.T) {
+	var req *http.Request
+	res := httptest.NewRecorder()
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(http.StatusOK)
+	res.WriteString(`{"ranking": ["Back to the Future", "Rambo"]}`)
+
+	c := http.Client{
+		Transport: newTransport(&req, res.Result(), nil)
+	}
+	r, err := c.Get("http://exmaple.com/movies/1985")
+	assert.Nil(t, err)
+	assert.Equal(t, 200, r.StatusCode)
 }
