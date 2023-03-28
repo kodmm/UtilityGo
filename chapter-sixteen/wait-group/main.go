9c28dcba89a18c3f51ce22fecb3cce6c9e26a3a1
kodmm <53547701+kodmm@users.noreply.github.com>
2023-03-28 14:03:08 +0900

    16.7 処理の待ち合わせをしたい(Future/Promiseパターン)

--- chapter-sixteen/wait-group/main.go
+++ chapter-sixteen/wait-group/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"sync"
 	"time"
 
@@ -49,4 +50,86 @@ func main() {
 	})
 	err := eg.Wait()
 	fmt.Println("Done all tasks", err)
+
+	fa, pa := MakeFuturePromise()
+	fb, pb := MakeFuturePromise()
+	fc, pc := MakeFuturePromise()
+	fd, pd := MakeFuturePromise()
+
+	go func() {
+		a := A()
+		pa.Submit(a)
+	}()
+	go func() {
+		b := B()
+		pa.Submit(b)
+	}()
+	go func() {
+		c := C(fa.Get(), fb.Get())
+		pa.Submit(c)
+	}()
+	go func() {
+		d := D(fa.Get(), fc.Get())
+		pa.Submit(d)
+	}()
+	log.Printf("d: %d\n", fd.Get())
+
+}
+
+func A() int {
+	time.Sleep(time.Second)
+	return 10
+}
+
+func B() int {
+	time.Sleep(time.Second * 2)
+	return 5
+}
+
+func C(a, b int) int {
+	time.Sleep(time.Second * 1)
+	return a + b
+}
+
+func D(a, c int) int {
+	time.Sleep(time.Second)
+	return a + c
+}
+
+type Future struct {
+	value int
+	wait  chan struct{}
+}
+
+func (f *Future) IsDone() bool {
+	select {
+	case <-f.wait:
+		return true
+	default:
+		return false
+	}
+}
+
+func (f *Future) Get() int {
+	<-f.wait
+	return f.value
+}
+
+type Promise struct {
+	f *Future
+}
+
+func (p *Promise) Submit(v int) {
+	p.f.value = v
+	close(p.f.wait)
+}
+
+func MakeFuturePromise() (*Future, *Promise) {
+	f := &Future{
+		wait: make(chan struct{}),
+	}
+	p := &Promise{
+		f: f,
+	}
+	return f, p
 }
